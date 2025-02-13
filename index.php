93d60ad556beafdc1cf80d352f2f02507e3ea3d5
Munoz <152181111+Kvrmea@users.noreply.github.com>
2025-02-13 18:55:20 +0100

    étape 2

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?php
-echo 'Salut ' . htmlspecialchars($_GET["first_name"]) . '!';
+// echo 'Salut ' . htmlspecialchars($_GET["first_name"]) . '!';
 
-// Déterminer le nom à afficher
-// if (isset($_GET["first_name"]) && !empty($_GET["first_name"])) {
-//     $name = htmlspecialchars($_GET["first_name"]);
-// } elseif (isset($_POST["first_name"]) && !empty($_POST["first_name"])) {
-//     $name = htmlspecialchars($_POST["first_name"]);
-// } else {
-//     $name = "anonyme";
-// }
+if (isset($_GET["first_name"]) && !empty($_GET["first_name"])) {
+    $name = htmlspecialchars($_GET["first_name"]);
+} elseif (isset($_POST["first_name"]) && !empty($_POST["first_name"])) {
+    $name = htmlspecialchars($_POST["first_name"]);
+} else {
+    $name = "anonyme";
+}
 
-// echo "Salut " . $name . "!";
-
+echo "Salut " . $name . "!";
 ?>
 
-<!-- <form action="index.php" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <p>Votre nom : <input type="text" name="first_name" /></p>
     <p><input type="submit" value="OK"></p>
-</form> -->
+</form>
+
+<!-- Vérification de GET : Si first_name est dans l’URL, on l'affiche.
+Sinon, vérification de POST : Si first_name est envoyé via le formulaire, on l'affiche.
+Si aucun nom n'est fourni, on affiche "anonyme".
+Le formulaire utilise $_SERVER["PHP_SELF"] pour éviter les erreurs si le fichier est renommé. -->
